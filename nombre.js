78ea6513652dfe20f93e5b801654502b8382cd59
coronaesmeralda080-cyber <coronaesmeralda080@gmail.com>
2025-11-28 16:00:45 -0600

    ciclos

--- nombre.js
+++ nombre.js
@@ -57,3 +57,82 @@ console.log(arreglo1)
  console.log(carro.motor.cilindros)
  console.log(moto.marca)
  
+for(let i = 1; i <= 10; i++)[
+[console.log(i)]
+]
+for( let i=100; i >= 1; i--)[
+    [console.log(i)]
+]
+
+function sumar(a, b){
+return a + b;
+}
+console.log(sumar(7,9))
+
+function multiplicar(a, b){
+    return a * b;
+}
+console.log(multiplicar(8, 3))
+
+let numeros = [ 5, 10 ,15, 20, 25];
+let sumar3 = 0;
+for(let i =0; i < numeros.length; i++) {
+    sumar3+= numeros[i]
+}
+console.log("la suma3 es:", sumar3);
+
+let numero = [ 6, 8, 5, 4, 10, 7];
+    let sumar4 = 0;
+    for(let i =0; i < numeros.length; i++) {
+ sumar4+= numero[i]
+    }
+    console.log("lasuma4 es:", sumar4);
+
+    function sumararreglo(arr){
+        let sumararr = 0;
+
+        for(let i = 0; i < arr.length; i++){
+            suma = arr[i];
+    }
+    return suma;
+    }
+    console.log(sumararreglo([10,20,30]));
+    console.log(sumararreglo([10,20,30,2]));
+    console.log(sumararreglo([10,20]));
+    console.log(sumararreglo([10,20,10]));
+    console.log(sumararreglo([10,20,12]));
+    console.log(sumararreglo([10,20,30]));
+    console.log(sumararreglo([10,20,30,31]));
+    console.log(sumararreglo([10,20,30,12]));
+    console.log(sumararreglo([10,20,30,10]));
+    console.log(sumararreglo([10,20,30,13]));
+    console.log(sumararreglo([10,20,30,5]));
+    console.log(sumararreglo([10,20,30,16]));
+    console.log(sumararreglo([10,20,30 ]));
+
+
+function restararreglo(arreglo){
+        let restar = 0;
+
+        for(let i = 0; i < arreglo.length; i++){
+            restar -= arreglo[i];
+    }
+    return restar;
+    }
+    console.log(restararreglo([15,30,45]));
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
